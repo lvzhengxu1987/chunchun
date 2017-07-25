d31b00a1cc6f42f8d579dc574007271ac8bd647d
lvzhengxu1987 <lvzhengxu1987@126.com>
2017-07-25 22:15:28 +0800

    update code

---
+++ source/dmoz_spider.py
@@ -0,0 +1,32 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'songchunjie'
+
+import scrapy
+import sys
+import urllib
+import urllib.request
+
+class DmozSpider(scrapy.Spider):
+    name = "dmoz"
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        "http://search.51job.com/list/010000,000000,0000,00,9,99,C%2520%25E5%25BC%2580%25E5%258F%2591,2,1.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare=",
+    ]
+    '''
+    def parse(self, response):
+        filename = response.url.split("/")[-2]
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+    '''
+    def  parse(self, response):
+        a=response.xpath('//*[@id="resultList"]/div[3]/p/span/a/@title').extract()
+        b=response.xpath('//*[@id="resultList"]/div[3]/p/span/a/@href').extract()
+        print a[0].encode('gbk')
+        print b[0].encode('gbk')
+        p = urllib.request.urlopen(b[0])
+        html1 = p.read()
+        s = etree.HTML(html1)
+        #print html1
+        b1=response.xpath('/html/body/div[2]/div[2]/div[3]/div[4]/div/*').extract()
+
+        print b1
