87b8bcaa9e4a0fc0a1d8f87458ad505d2595bd7f
lvzhengxu1987 <lvzhengxu1987@126.com>
2017-09-28 17:35:12 +0800

    pthrade_pool update

--- pthread/pthreadpool/main.c
+++ pthread/pthreadpool/main.c
@@ -1,4 +1,4 @@
-#include "threadpool.h"
+#include "pthreadpool.h"
 
 void * work(void * arg)
 {
@@ -10,10 +10,13 @@ int main()
 {
     struct threadpool *pool;
     pool = threadpool_init(10,20);
+    printf("threadpool_init   OK \n");
     int num = 0 ;
     for(num = 0; num < 40; num++)
     {
-        threadpool_work(pool, work, num);
+        threadpool_work(pool, work, &num);
     }
+    sleep(5);
+    threadpool_destroy(pool);
     return 0;
 }
--- pthread/pthreadpool/pthreadpool.c
+++ pthread/pthreadpool/pthreadpool.c
@@ -1,24 +1,31 @@
-#include"threadpool.h"
+#include"pthreadpool.h"
+#include <pthread.h>
 
-int threadpool_work(struct threadpoo* pool, void * (*callback_function)(void * arg), void * arg)
+int threadpool_work(struct threadpool* pool, void * (*callback_function)(void * arg), void * arg)
 {
+    printf("begin malloc\n");
+    pthread_mutex_trylock(&(pool->mutex));
+    printf("begin malloc111111\n");
     pthread_mutex_lock(&(pool->mutex));
     /*先判断现在是否满足条件*/
-    if((pool->work_cur_num == work_max_num )&& !pool->pool_close || pool->pthread_close)
+    printf("begin malloc111111\n");
+    if((pool->work_cur_num == pool->work_max_num)&& !pool->pool_close || pool->pthread_close)
     {
+        printf("begin malloc\n");
         /*pool->mutex该参数保证等待时是锁状态*/
-        pthread_cont_wait(&(pool->work_not_full),&(pool->mutex));
+        pthread_cond_wait(&(pool->work_not_full),&(pool->mutex));
     }
     if(pool->pool_close || pool->pthread_close)
     {
         pthread_mutex_unlock(&(pool->mutex));
         return -1;
     }
-    struct job *pjob = (struct job*)malloc(sizeof(struct job))
+    printf("begin malloc\n");
+    struct job *pjob = (struct job*)malloc(sizeof(struct job));
     if(NULL == pjob)
     {
         printf("malloc pjob faile\n");
-        ruturn -1;
+        return  -1;
     }
     pjob->callback_function = callback_function;
     pjob->arg = arg;
@@ -26,10 +33,14 @@ int threadpool_work(struct threadpoo* pool, void * (*callback_function)(void * a
     if(pool->head == NULL)
     {
         pool->head = pool->tail = pjob;
+        /*通知线程池有任务到来*/
+        pthread_cond_broadcast(&(pool->work_not_empty));
+        printf("等待任务\n");
     }
     else
     {
-        pool->tail->next = pjob;
+        pool->head->next = pjob;
+        pool->tail = pjob;
 
     }
     pool->work_cur_num ++;
@@ -42,8 +53,8 @@ struct threadpool * threadpool_init(int thread_num, int work_max_num)
     struct threadpool *pool = NULL;
     do
     {
-        pool = malloc(sizeof(struct threadpoo));
-        if(NULL == poll)
+        pool = malloc(sizeof(struct threadpool));
+        if(NULL == pool)
         {
             printf("malloc pthreadpool fail\n");
             break;
@@ -53,7 +64,9 @@ struct threadpool * threadpool_init(int thread_num, int work_max_num)
         pool->work_cur_num = 0;
         pool->pthread_close = 0;
         pool->pool_close = 0;
-        if(pthread_mutex_init(&(pool->mutex), NULL) <0)
+        pool->head = NULL;
+        pool->tail = NULL;
+        if (pthread_mutex_init(&(pool->mutex), NULL))
         {
             printf("pthread_mutex_init faile\n");
             break;
@@ -73,7 +86,7 @@ struct threadpool * threadpool_init(int thread_num, int work_max_num)
             printf("pthread_cond_init work_not_fullfaile\n");    
             break;
         }
-        pool->pthreads = malloc(sizeof(struct threadpoo)*thread_num);
+        pool->pthreads = malloc(sizeof(struct threadpool)*thread_num);
         if(NULL == pool->pthreads)
         {
             printf("maoolc pool->pthreads faile\n");
@@ -82,19 +95,95 @@ struct threadpool * threadpool_init(int thread_num, int work_max_num)
         int i;
         for(i = 0; i< thread_num; i++)
         {
-            pthread_create(pool->pthreads[i],NULL, threadpool_function, NULL);
+            pthread_create(&(pool->pthreads[i]),NULL, threadpool_function, (void *)pool);
         }
 
     }
     while(0);
-    return NULL
+    return NULL;
 }
 int threadpool_destroy(struct threadpool *pool)
 {
+    pthread_mutex_lock(&(pool->mutex));
+    if(pool->pool_close || pool->pthread_close)
+    {
+        pthread_mutex_unlock(&(pool->mutex));
+        return -1;
+    }
+    pool->pthread_close = 1;/*关闭标志*/
+    while(1)
+    {
+        pthread_cond_wait(&(pool->work_not_empty), &(pool->mutex));    
+    }
+    pool->pool_close = 1;      //置线程池关闭标志
+    pthread_mutex_unlock(&(pool->mutex));
+    /*一下两行不理解*/
+    pthread_cond_broadcast(&(pool->work_not_empty));
+    pthread_cond_broadcast(&(pool->work_not_full));
+    int i;
+    for(i =0; i < pool->thread_num; i++)
+    {
+        pthread_join(pool->pthreads[i], NULL);
+    }
+    pthread_mutex_destroy(&(pool->mutex));
+    pthread_cond_destroy(&(pool->work_empty));
+    pthread_cond_destroy(&(pool->work_not_empty));
+    pthread_cond_destroy(&(pool->work_not_full));
+    struct job *p;
+    while(pool->head!=NULL)
+    {
+        p = pool->head;
+        pool->head = p->next;
+        free(p);
+    }
+    free(pool);
+    return 0;
 
 }
 void* threadpool_function(void* arg)
 {
+    struct threadpool * pool = (struct threadpool *)arg;
+    struct job *pjob = NULL;
+    while(1)
+    {
+        pthread_mutex_trylock(&(pool->mutex));
+        pthread_mutex_lock(&(pool->mutex));
+        /*线程池关闭或者也没有需要处理的任务*/
+        while((pool->work_cur_num ==0) && !pool->pool_close)
+        {
+            pthread_cond_wait(&(pool->work_not_empty),&(pool->mutex));
+        }
+        if(pool->pool_close)
+        {
+            pthread_mutex_unlock(&(pool->mutex));    
+            pthread_exit(NULL);
+        }
+        pjob = pool->head;
+        if(pool->work_cur_num == 0)
+        {
+            pool->head = pool->tail = NULL;
+        }
+        else
+        {
+            pool->head = pjob->next;
+        }
+        if(pool->work_cur_num == 0)
+        {
+            /*没有任务需要处理时如何做*/
+            pthread_cond_signal(&(pool->work_empty));
+
+        }
+        if(pool->work_cur_num == pool->work_max_num - 1)
+        {
+            /*当前任务达到最大时，如何控制*/
+            pthread_cond_broadcast(&(pool->work_not_full));    
+        }
+        pool->work_cur_num--;
+        pthread_mutex_unlock(&(pool->mutex));
+        (*(pjob->callback_function))(pjob->arg);
+        free(pjob);
+        pjob = NULL;
+    }
 
 }
 
